8d7f5386fc71850aba86dc08a7c9de3ca42a6482
MiZone <mizonerom@hotmail.com>
2018-12-26 10:16:34 +0800

    。

---
+++ MZcms.Web/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MZcms.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteTable.Routes.IgnoreRoute("Areas/");
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void SetupResolveRules(ContainerBuilder builder)
+        {
+            Assembly assembly = Assembly.Load("MZcms.IServices");
+            Assembly assembly1 = Assembly.Load("MZcms.Services");
+            Assembly[] assemblyArray = new Assembly[] { assembly, assembly1 };
+            builder.RegisterAssemblyTypes(assemblyArray).Where((Type t) => t.Name.EndsWith("Service")).AsImplementedInterfaces<object>();
+        }
+
+    }
+}
